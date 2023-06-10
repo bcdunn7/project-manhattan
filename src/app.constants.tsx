fe8781aa4adb06d37b9efdad8931853fec046fe8
Bryce Dunn <bryce.cc.dunn@gmail.com>
2023-06-10 12:20:12 -0500

    belt badge, courses page, and quote refactoring

---
+++ src/app.constants.tsx
@@ -0,0 +1,24 @@
+import { BeltRank } from "./app.models";
+
+export const BELT_COLORS_BG_TAILWIND = {
+  white: 'white',
+  yellow: 'yellow-300',
+  orange: 'orange-300',
+  purple: 'purple-300',
+  blue: 'blue-300',
+  green: 'green-300',
+  brown: 'amber-950',
+  'brown-2': 'amber-950',
+  red: 'red-300',
+  'red-2': 'red-300',
+  'red-3': 'red-300',
+  'black-temp': 'black',
+  'black-1': 'black',
+  'black-2': 'black',
+  'black-3': 'black',
+  'black-4': 'black',
+  'black-5': 'black',
+  'black-6': 'black',
+  'black-7': 'black',
+  'black-8': 'black',
+}
