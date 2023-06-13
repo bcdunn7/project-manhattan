15f9781091bf855ba67022d6ced4cc765459e013
Bryce Dunn <bryce.cc.dunn@gmail.com>
2023-06-13 17:53:31 -0500

    starting work on hotkey

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,4 +1,4 @@
-import { Slot, component$, $ } from '@builder.io/qwik';
+import { Slot, component$, $, useVisibleTask$, useSignal } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 import { InitialValues, SubmitHandler, useForm } from '@modular-forms/qwik';
 import Footer from '~/components/footer';
@@ -23,12 +23,23 @@ export default component$(() => {
     }
   })
 
+  // TODO: is this worth it? styling for focus/cmd+k; not done either way
+  useVisibleTask$(() => {
+    window.addEventListener('keydown', (keydown) => {
+
+      if (keydown.key?.toLowerCase() === 'k' && keydown.metaKey) {
+        console.log(keydown);
+        (document.querySelector('#formDiv') as HTMLElement).focus();
+      }
+    })
+  })
+
   return (
     <div class='flex flex-col h-screen'>
       <Header />
-      <div class='mt-4 h-18 flex items-center'>
+      <div id='formDiv' class='mt-4 h-18 flex items-center'>
         <Form onSubmit$={handleSubmit} class='w-full flex justify-between   gap-4 mx-16'>
-          <Field name='query' >
+          <Field name='query'>
             {(field, props) => <input class='flex grow h-12 border rounded border-purple-800 px-4' {...props} type='text' />}
           </Field>
           <button class='border rounded border-purple-800 w-20' type='submit' disabled={frontierHelperForm.submitting}>
