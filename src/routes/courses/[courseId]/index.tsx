db64749eab8d0f8da9925372f58815c7fdaef622
Bryce Dunn <bryce.cc.dunn@gmail.com>
2023-06-13 18:05:51 -0500

    course details page styling

--- src/routes/courses/[courseId]/index.tsx
+++ src/routes/courses/[courseId]/index.tsx
@@ -2,6 +2,8 @@ import { component$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 import { Course } from '../models';
 import { Student } from '~/routes/students/models';
+import PageTitle from '~/components/page-title';
+import LevelBadge from '~/components/level-badge';
 
 export const useCourse = routeLoader$(async ({ params }) => {
   const courseRes = await fetch(`http://localhost:3000/courses/${params.courseId}?_expand=instructor`);
@@ -17,24 +19,37 @@ export default component$(() => {
   const courseData = useCourse();
 
   return (
-    <div>
-      <h1>Course</h1>
+    <div class='m-4 w-full'>
+      <PageTitle>Course</PageTitle>
 
-      <span>{courseData.value.course.label}</span>
-      <span>{courseData.value.course.name}</span>
-      {/* TODO: capitalize */}
-      <span class='capitalize'>
-        {courseData.value.course.level}
-      </span>
-      <div>
-        {courseData.value.course.instructor?.name}
-      </div>
-      <div>
-        <ul>
-          {courseData.value.students.map((student) => {
-            return <li>{student.name}</li>
-          })}
-        </ul>
+      <a href='/courses' class='hover:underline'>Back to Course List</a>
+
+      <div class='w-full px-64'>
+        <div class='flex items-center gap-2'>
+          <div class='text-2xl'>{courseData.value.course.name}</div>
+          <div class='flex justify-center bg-gray-100 border rounded px-2 py-0.5'>
+            {courseData.value.course.label}
+          </div>
+          <LevelBadge level={courseData.value.course.level} />
+        </div>
+        <div class='my-4 text-lg'>
+          Instructor: {courseData.value.course.instructor?.name}
+        </div>
+        <div class='my-4 text-lg'>
+          Students{
+            !!courseData.value.students.length
+            && ` (${courseData.value.students.length})`
+          }:
+          {
+            courseData.value.students.length
+              ? <ul>
+                {courseData.value.students.map((student) => {
+                  return <li>- {student.name}</li>
+                })}
+              </ul>
+              : ' None 😭'
+          }
+        </div>
       </div>
     </div>
   )
